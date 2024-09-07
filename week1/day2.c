12d0374f4c1e3f56de88c2fc18a06c35d471acb9
Jack Ohrn <jackohrn1@gmail.com>
2024-09-07 14:05:40 -0400

    new

--- week1/day2.c
+++ week1/day2.c
@@ -5,6 +5,6 @@ int main(int argc, char *argv[])
 {
     // TODO: Write your code here
     printf("\t hello \t yuy");
-    printf("\"im horny\"-jacko hrn");
+    printf("\"im jack\"-jacko hrn");
     return 0;
 }
