cf6697ebff122676b7d3e95b507c0b9e98e6f2fa
Jack Ohrn <jackohrn1@gmail.com>
2024-09-07 15:09:50 -0400

    LF's

--- week1/day2.c
+++ week1/day2.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
-    // TODO: Write your code here
-    printf("\t hello \t yuy");
-    printf("\"im jack\"-jacko hrn");
+    char box[] = "final number is:\t";
+    double x = 1111111.23;
+    double y = 212123.131231;
+    double xy = (x * y) / 2.12819;
+    printf("%s, %.20lf", box, xy);
+
     return 0;
 }
