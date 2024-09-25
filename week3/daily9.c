5e43eb25eed53b8d393063b35e6cfc5a415b6767
Jack Ohrn <jackohrn1@gmail.com>
2024-09-25 10:55:49 -0400

    final daily9.c

--- week3/daily9.c
+++ week3/daily9.c
@@ -1,13 +1,20 @@
 /*
  *Author: Jack Ohrn
  *Sept. 25th, 2024
- *Description
+ *Program asks for a positive int.--> program decrements input to zero,
+ *then increments from zero back up to original int value without recursion.
  */
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ *This function takes an integer argument from main
+ *and decrements down to zero. Return type is void.
+ */
 void loop_down_to_zero(int num);
-
+/*
+ *This function takes original integer from main, and increments from zero back to the passed int.
+ */
 void loop_up_to_int(int num);
 
 int main(void)
