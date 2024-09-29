84257d1ef44e154b1d35f53275fd6dc4ace19550
Jack Ohrn <jackohrn1@gmail.com>
2024-09-29 18:18:03 -0400

    cleaned

---
+++ week4/ascii.c
@@ -0,0 +1,18 @@
+/*
+ *Author: Jack Ohrn
+ *Date
+ *Description
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int x = 0;
+    do
+    {
+        x++;
+        printf("%d \n", x);
+    } while (x > 0);
+    return 0;
+}
