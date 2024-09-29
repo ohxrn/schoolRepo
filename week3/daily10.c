d4e3979d8692a8a932302e3f7d37a18c3ba4280c
Jack Ohrn <jackohrn1@gmail.com>
2024-09-29 12:58:31 -0400

    daily 10

---
+++ week3/daily10.c
@@ -0,0 +1,37 @@
+/*
+ *Author: Jack Ohrn
+ *Sept. 29th
+ *This program prompts the user to input a negativer integer,
+ *and uses input validation to ask a user to re enter if int is positive, or a character, etc.
+ -
+*Time spent: ~15 Minutes
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+void clear_buffer(void);
+
+int main(void)
+{
+    int num;
+    printf("insert a negative integer: \n");
+    int result = scanf("%d", &num);
+    clear_buffer();
+    while (result == 0 || num >= 0)
+    {
+        printf("int needs to be negative. Enter again \n");
+        result = scanf("%d", &num);
+        clear_buffer();
+    }
+    printf("the negative integer was: %d \n", num);
+    return 0;
+}
+void clear_buffer(void)
+{
+    char c;
+    scanf("%c", &c);
+    while (c != '\n')
+    {
+        scanf("%c", &c);
+    }
+}
